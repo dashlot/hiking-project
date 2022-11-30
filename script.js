1150c6f722f7f5fc845eb4b35d414e63b9949cb2
dashlot <daschenki@yandex.ru>
2022-11-30 19:40:32 +0400

    Finish footer

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function fillCardFromJson(jsonObject) {
+    let result =
+    '<div class="card">'+
+        '<img src="' + jsonObject.img + '">'+
+        '<h3>' + jsonObject.title + '</h3>'+
+        '<p>' + jsonObject.text + '</p>'+
+    '</div>';
+    return result
+}
+
+
+
 window.onload = function() {
     const caller = document.querySelector('.button form button');
     caller.addEventListener('click', function() {
@@ -13,18 +25,33 @@ window.onload = function() {
             title: "Lorem ipsum",
             text: "It's astonished air. Smell it!"
         }
-
         jsonContent[2] = {
             img: "https://www.bergans.com/pub_images/original/fasthiking1.jpg",
             title: "Lorem ipsum",
             text: "BlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBlaBla" 
         }
-        const divContent = document.querySelector('.content1');
-            divContent.innerHTML = "<div class=\"card\"> <img src=\"" + jsonContent[0].img + "\"><h3> \"" + jsonContent[0].title + "\" </h3> <p> \"" + jsonContent[0].text + "\"</p></img></div>";
-            const divContent2 = document.querySelector('.content2');
-            divContent2.innerHTML = "<div class=\"card\"> <img src=\"" + jsonContent[1].img + "\"><h3> \"" + jsonContent[1].title + "\" </h3> <p> \"" + jsonContent[1].text + "\"</p></img></div>"
-            const divContent3 = document.querySelector('.content3');
-            divContent3.innerHTML = "<div class=\"card\"> <img src=\"" + jsonContent[2].img + "\"><h3> \"" + jsonContent[2].title + "\" </h3> <p> \"" + jsonContent[2].text + "\"</p></img></div>"
+
+        for (let i = 0; i < 3; i++) {
+            const divContent = document.querySelector('.cards.extra');
+            divContent.innerHTML += fillCardFromJson(jsonContent[i]);
+        }
+
+        // const divContent = document.querySelector('.content1');
+        // // const elem0 = jsonContent[0];
+        // divContent.innerHTML = fillCardFromJson(jsonContent[0]);
+
+
+        // const divContent2 = document.querySelector('.content2');
+        // const elem1 = jsonContent[1];
+        //     divContent2.innerHTML = fillCardFromJson(elem1);
+        
+        // const divContent3 = document.querySelector('.cards.extra');
+        //     divContent3.innerHTML =
+        // '<div class="card">'+ 
+        //     '<img src="' + jsonContent[2].img + '">'+
+        //     '<h3>'+ jsonContent[2].title + '</h3>'+
+        //     '<p>' + jsonContent[2].text + '</p>'+
+        // '</div>'
     });
     
     let subscribe = document.querySelector('.child2 form input');
@@ -32,6 +59,7 @@ window.onload = function() {
         alert("Suckmydick")
         }
     )
+//     debugger;
 }
 
 
