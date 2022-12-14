43df216b4a0c763ad5f34375186908d1525381bd
Dasha Karpova <i.am.not.pursell@gmail.com>
2022-12-14 20:35:51 +0400

    feat: add the function that counts number of likes and dislikes

--- script.js
+++ script.js
@@ -17,15 +17,7 @@ function addContentFromJson(jsonObject) {
     return result
 };
 
-$('.like button').click(function() {
-    alert("You like it!")
-    })
-
-$('.dislike button').click(function() {
-    alert("You dislike it!")
-})
-
-window.onload = function(){ 
+window.onload = function(){
     let arrayUrls = [];
     arrayUrls[0] = "https://run.mocky.io/v3/59e20ba9-3eed-4bc9-aa63-a0bd5a67b08c";
     arrayUrls[1] = "https://run.mocky.io/v3/1533fe10-19c9-437f-a55d-9a8dc19e55a5";
@@ -34,8 +26,19 @@ window.onload = function(){
     arrayUrls[4] = "https://run.mocky.io/v3/40699510-f46e-4b4d-b088-24f98cbd873d";
     arrayUrls[5] = "https://run.mocky.io/v3/5dff2a11-7e41-4769-b0be-04d81b10857d";
     
-    let callTheButton = $('.button form button');
-    callTheButton.on('click', function() {
+    let countOfLikes = 0;
+    let countOfDislikes = 0;
+
+    $('.like button').click(function() {
+        countOfLikes +=1;
+        console.log(`Likes = ${countOfLikes}`);
+    })
+    $('.dislike button').click(function() {
+        countOfDislikes +=1;
+        console.log(`Dislikes = ${countOfDislikes}`);
+    }) 
+
+    $('.button form button').on('click', function() {
         let loadingIcon = $('#loading-icon')
         loadingIcon.html(`<i class="fa-solid fa-spinner"></i>`)
         function getRandomInt(min, max) {
@@ -57,16 +60,18 @@ window.onload = function(){
                 let newCon = oldCon +
                 addContentFromJson(jsonContent[i])
                 jqObj.html(newCon);
-            }  
+            } 
+
+            console.log('before like button adding click handler')
+            $('.like button').unbind().click(function() {
+                alert("You like it!")
+            })
+            
+            $('.dislike button').unbind().click(function() {
+                alert("You dislike it!")
+            })
         })
 
-        $('.like button').click(function() {
-        alert("You like it!")
-    })
-    
-    $('.dislike button').click(function() {
-        alert("You dislike it!")
-    })
     })
 
     let subscribe = $('.child2 form input');
@@ -80,6 +85,8 @@ window.onload = function(){
             let divCity = $('#city-from-api');
             divCity.html(`<p>${data.city}</p>`); 
         });
+
+
     })
 }
 
